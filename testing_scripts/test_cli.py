4a6ae1f5d143952510b01ce4914b73d3a0a5c191
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-11-18 13:59:06 +0200

    Adding automated building and testing environment and a basic test for CLI

---
+++ testing_scripts/test_cli.py
@@ -0,0 +1,42 @@
+import os
+from pathlib import Path
+
+import pytest
+from click.testing import CliRunner
+
+from diff_res_bpsim import cli
+
+
+@pytest.fixture
+def assets_path(request) -> Path:
+    entry_path: Path
+    if os.path.basename(os.getcwd()) == 'testing_scripts':
+        entry_path = Path('assets')
+    else:
+        entry_path = Path('testing_scripts/assets')
+
+    def teardown():
+        output_path = entry_path / 'PurchasingExample.csv'
+        if output_path.exists():
+            os.remove(output_path)
+    request.addfinalizer(teardown)
+
+    return entry_path
+
+
+def test_start_simulation(assets_path):
+    runner = CliRunner()
+    model_path = assets_path / 'PurchasingExampleQBP.bpmn'
+    json_path = assets_path / 'PurchasingExampleQBP.json'
+    output_path = assets_path / 'PurchasingExample.csv'
+    result = runner.invoke(cli, [
+        'start-simulation',
+        '--bpmn_path', str(model_path),
+        '--json_path', str(json_path),
+        '--log_out_path', str(output_path),
+        '--total_cases', '20'
+    ])
+    assert result.exit_code == 0, f'Output: {result.output}'
+
+    with output_path.open('r') as f:
+        assert len(f.readlines()) > 1, 'Output log must have more than 1 line'
