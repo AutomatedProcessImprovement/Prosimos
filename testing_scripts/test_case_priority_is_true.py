e2cedb7948ba8559b7745a56d5304f5f15d4ccdb
Iryna Halenok <iryna.halenok@gmail.com>
2023-01-17 17:02:47 +0200

    #43 - Add test cases for AND and OR rules

--- testing_scripts/test_case_priority_is_true.py
+++ testing_scripts/test_case_priority_is_true.py
@@ -1,5 +1,13 @@
 import pytest
-from bpdfr_simulation_engine.prioritisation import PrioritisationRule
+from bpdfr_simulation_engine.prioritisation import (
+    AndPrioritisationRule,
+    OrPrioritisationRule,
+    PrioritisationRule,
+)
+
+BUSINESS = "Business"
+REGULAR = "Regular"
+INF = "inf"
 
 discrete_test_cases = [
     ([0, 100], 50, True),
@@ -7,8 +15,8 @@ discrete_test_cases = [
     ([200, 500], 239, True),
     ([200, 500], 128, False),
     ([200, 500], 600, False),
-    ([100, "inf"], 890, True),
-    ([100, "inf"], 90, False),
+    ([100, INF], 890, True),
+    ([100, INF], 90, False),
 ]
 
 
@@ -28,8 +36,8 @@ def test_discrete_rule_correct(rule_range, test_value, expected_result):
 
 
 continuous_test_cases = [
-    ("Business", "Business", True),
-    ("Business", "Regular", False),
+    (BUSINESS, BUSINESS, True),
+    (BUSINESS, REGULAR, False),
 ]
 
 
@@ -46,3 +54,78 @@ def test_continuous_rule_correct(rule_value, test_value, expected_result):
 
     # ====== ASSERT ======
     assert actual_result == expected_result
+
+
+and_test_cases = [
+    (90, BUSINESS, True),
+    (190, BUSINESS, False),
+    (79, REGULAR, False),
+    (190, REGULAR, False),
+]
+
+
+@pytest.mark.parametrize(
+    "loan_amount_value, category_value, expected_result",
+    and_test_cases,
+)
+def test_and_combination_correct(loan_amount_value, category_value, expected_result):
+    # ====== ARRANGE ======
+    and_rule = _create_and_rule_with_two_subrule([10, 100], BUSINESS)
+
+    # ====== ACT ======
+    actual_result = and_rule.is_and_rule_true(
+        {
+            "loan_amount": loan_amount_value,
+            "category": category_value,
+        }
+    )
+
+    # ====== ASSERT ======
+    assert actual_result == expected_result
+
+
+or_test_cases = [
+    ([10, 300], 280, BUSINESS, True),
+    ([10, 100], 239, BUSINESS, True),
+    ([10, 100], 73, BUSINESS, True),
+    ([10, 100], 130, REGULAR, False),
+]
+
+
+@pytest.mark.parametrize(
+    "range_rule1, loan_amount_value, category_value, expected_result",
+    or_test_cases,
+)
+def test_or_combination(
+    range_rule1, loan_amount_value, category_value, expected_result
+):
+    # ====== ARRANGE ======
+    or_rule = _create_or_rule_with_two_subrule(
+        [range_rule1, BUSINESS], [[200, 500], BUSINESS]
+    )
+
+    # ====== ACT ======
+    actual_result = or_rule.is_or_rule_true(
+        {"loan_amount": loan_amount_value, "category": category_value}
+    )
+
+    # ====== ASSERT ======
+    assert actual_result == expected_result
+
+
+def _create_and_rule_with_two_subrule(discrete_range, continuous_value):
+    rule1 = PrioritisationRule("loan_amount", "in", discrete_range)
+    rule2 = PrioritisationRule("category", "=", continuous_value)
+
+    and_rule = AndPrioritisationRule([rule1, rule2])
+
+    return and_rule
+
+
+def _create_or_rule_with_two_subrule(values_rule1, values_rule2):
+    and_rule1 = _create_and_rule_with_two_subrule(values_rule1[0], values_rule1[1])
+    and_rule2 = _create_and_rule_with_two_subrule(values_rule2[0], values_rule2[1])
+
+    or_rule = OrPrioritisationRule([and_rule1, and_rule2])
+
+    return or_rule
