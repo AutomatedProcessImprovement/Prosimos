52f9f189742ecdae249de2587172003a4f7bfa0b
Iryna Halenok <iryna.halenok@gmail.com>
2023-03-06 19:05:52 +0200

    #52 - Preserve folder structure, update process files setup

--- performance_exp/events/testing_files.py
+++ performance_exp/events/testing_files.py
@@ -2,6 +2,11 @@ process_files = {
     "events_exp": {
         "bpmn": "input/batch-example-end-task.bpmn",
         "json": "input/batch-example-with-batch.json",
+        "results_folder": "input/results",
+        "start_datetime": "2022-06-21 13:22:30.035185+03:00",
+        "total_cases": 1000,
+        "disc_params": [60, 0.1, 0.9, 0.6, True],
+        "number_of_added_events": 9,  # should be equal to the number of sequence flows in the BPMN model
         "results_folder": "results",
         "start_datetime": "2022-06-21 13:22:30.035185+03:00",
         "total_cases": 1000,
