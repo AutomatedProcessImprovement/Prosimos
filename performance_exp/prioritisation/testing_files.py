4e09a372a6e6ae7ad2fad9c9dc95d9d4295fa64a
Iryna Halenok <iryna.halenok@gmail.com>
2023-03-06 19:18:47 +0200

    #53 - Run experiments with adding diifferent number of priority levels

---
+++ performance_exp/prioritisation/testing_files.py
@@ -0,0 +1,10 @@
+process_files_setup = {
+    "simple_example": {
+        "bpmn": "simple_example/batch-example-end-task.bpmn",
+        "json": "simple_example/batch-example-with-batch.json",
+        "results_folder": "simple_example/results",
+        "start_datetime": "2022-06-21 13:22:30.035185+03:00",
+        "total_cases": 2000,
+        "disc_params": [60, 0.1, 0.9, 0.6, True],
+    },
+}
