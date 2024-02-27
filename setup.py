9d6342f4e075bcd667610fe0991393207971dc92
Jannis Rosenbaum <j.ro@dock51.de>
2024-02-27 22:21:02 +0200

    include package

--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='DiffResBP_Simulator',
-    version='0.1.0',
+    name="DiffResBP_Simulator",
+    version="0.1.0",
     include_package_data=True,
-    package_dir={'': '.'},
-    install_requires=[
-        'click',
-        'simpy'
-    ],
+    package_dir={
+        "": ".",
+        "bpdfr_simulation_engine": "bpdfr_simulation_engine",
+    },
+    install_requires=["click", "simpy"],
     entry_points={
-        'console_scripts': [
-            'diff_res_bpsim = diff_res_bpsim:cli',
+        "console_scripts": [
+            "diff_res_bpsim = diff_res_bpsim:cli",
         ]
-    }
+    },
 )
